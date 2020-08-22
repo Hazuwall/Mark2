fa94297f6d2294734ac12b03c9bfb5d11c6a860e
Hazuwall <yalutorovsk4@gmail.com>
2020-08-23 00:52:33 +0500

    Debug role disputes, edit roles api

--- Server/Roles/IRoleDisputeFactory.cs
+++ Server/Roles/IRoleDisputeFactory.cs
@@ -6,5 +6,6 @@ namespace Server.Roles
     public interface IRoleDisputeFactory
     {
         IRoleDispute Create(Guid claimant, Guid defendant, Role role);
+        IRoleDispute GetLast(Role role);
     }
 }
--- Server/Roles/RoleDisputeFactory.cs
+++ Server/Roles/RoleDisputeFactory.cs
@@ -1,23 +1,36 @@
 ﻿using Common;
 using Common.Services;
 using System;
+using System.Collections.Generic;
 
 namespace Server.Roles
 {
     public class RoleDisputeFactory : IRoleDisputeFactory
     {
-        private readonly IEventPublisher _raiser;
+        private readonly IEventPublisher _publisher;
         private readonly IClientRoleRegistry _registry;
+        private readonly Dictionary<Role, IRoleDispute> _disputes;
 
-        public RoleDisputeFactory(IEventPublisher raiser, IClientRoleRegistry registry)
+        public RoleDisputeFactory(IEventPublisher publisher, IClientRoleRegistry registry)
         {
-            _raiser = raiser;
+            _publisher = publisher;
             _registry = registry;
+            _disputes = new Dictionary<Role, IRoleDispute>()
+            {
+                {Role.Reader, null },
+                { Role.Writer, null },
+                { Role.Admin, null }
+            };
         }
 
         public IRoleDispute Create(Guid claimant, Guid defendant, Role role)
         {
-            return new RoleDispute(claimant, defendant, role, _registry, _raiser);
+            return _disputes[role] = new RoleDispute(claimant, defendant, role, _registry, _publisher);
+        }
+
+        public IRoleDispute GetLast(Role role)
+        {
+            return _disputes[role];
         }
     }
 }
